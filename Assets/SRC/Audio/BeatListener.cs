ce0769486cab5cfb0c67c3aeaab21e3197e2b822
Богдан Герасимчук <bohdan.herasimchuk@zagravagames.com>
2023-04-02 02:27:43 +0300

    Added env beat catch reactions

--- Assets/SRC/Audio/BeatListener.cs
+++ Assets/SRC/Audio/BeatListener.cs
@@ -5,6 +5,14 @@ using UnityEngine.Events;
 
 public class BeatListener : MonoBehaviour
 {
+    public AnimationClip audioDist;
+    public AnimationClip enviromentBeatCatched;
+    private Animation animation;
+    public bool defaultAnim = false; //TEMPORARY
+    private void Awake()
+    {
+        animation = GetComponent<Animation>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -25,21 +33,28 @@ public class BeatListener : MonoBehaviour
     }
     public void BeatCatched()
     {
+        if (!defaultAnim)
+        {
+            return;
+        }
         var transform = GetComponent<Transform>();
         transform.localScale = new Vector3(1.0f, 0.50f, 1.0f);
         Invoke(nameof(ResetScale), 0.1f);
     }
     public void BeatMissedSoundDist()
     {
-        var lossAnim = GetComponent<Animation>();
-        if (lossAnim)
-        {
-            lossAnim.Play();
-        }
+        animation.clip = audioDist;
+        animation.Play();
     }
     public void ResetScale()
     {
         var transform = GetComponent<Transform>();
         transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    public void EnviromentBeatCatched()
+    {
+        animation.clip = enviromentBeatCatched;
+        animation.Play();
     }
 }
