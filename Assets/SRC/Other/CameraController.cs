ea5a18132803ebd0e9c8f864bebb9d259da23203
Victor Bykov <victor.bykov@zagravagames.com>
2023-04-01 17:44:05 +0300

    Added  Camera Controller

---
+++ Assets/SRC/Other/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public AnimationClip MissedBeat;
+    public AnimationClip GameOver;
+    private Animation _cameraanimation;
+
+    public void Awake() 
+    {
+        _cameraanimation = GetComponent<Animation>();
+    }
+
+   public void CameraMissedBeat()
+    {
+        _cameraanimation.clip = MissedBeat;
+        _cameraanimation.Play();
+    }
+
+   public void CameraGameOver()
+    {
+        _cameraanimation.clip = GameOver;
+        _cameraanimation.Play();
+    }
+
+}
