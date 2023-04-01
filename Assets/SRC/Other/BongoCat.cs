6976f009c35ced788fac0d4c512defbb2449e880
Богдан Герасимчук <bohdan.herasimchuk@zagravagames.com>
2023-04-02 01:44:38 +0300

    Added cat idle

--- Assets/SRC/Other/BongoCat.cs
+++ Assets/SRC/Other/BongoCat.cs
@@ -6,18 +6,43 @@ public class BongoCat : MonoBehaviour
 {
     public Sprite catDown;
     public Sprite catUp;
+    public AnimationClip catIdleClip;
+    public float timeToStartIdle = 1f;
+
     private SpriteRenderer sRenderer;
+    private Animation animation;
+    private float idleTime = 0f;
+    private bool isInIdle = false;
+
+    public void Update()
+    {
+        idleTime += Time.deltaTime;
+        if(!isInIdle && idleTime >= timeToStartIdle)
+        {
+            SetIdle();
+        }
+    }
     public void Awake()
     {
         sRenderer = GetComponent<SpriteRenderer>();
+        animation = GetComponent<Animation>();
     }
     public void CatUp()
     {
+        idleTime = 0f;
         sRenderer.sprite = catUp;
     }
     public void CatDown()
     {
+        animation.Stop();
+        idleTime = 0f;
         sRenderer.sprite = catDown;
         Invoke(nameof(CatUp), 0.1f);
+    }
+    public void SetIdle()
+    {
+        isInIdle = !isInIdle;
+        animation.clip = catIdleClip;
+        animation.Play();
     }
 }
