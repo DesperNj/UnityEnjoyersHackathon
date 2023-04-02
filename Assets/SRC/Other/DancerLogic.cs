fe6a52560f2f233621e386ec442c0718bf7d58f6
Богдан Герасимчук <bohdan.herasimchuk@zagravagames.com>
2023-04-02 16:37:26 +0300

    Rix added

---
+++ Assets/SRC/Other/DancerLogic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+public class DancerLogic : MonoBehaviour
+{
+    public SkeletonAnimation skeleton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
